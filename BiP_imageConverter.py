40f7ddbd5da05bc2530a9d9cb3f878bac89a01fb
Corentin Monmasson (aka Bftech) <corentin.monmasson@gmail.com>
2017-07-27 18:21:16 +0200

    alpha

--- BiP_imageConverter.py
+++ BiP_imageConverter.py
@@ -1,8 +1,12 @@
 # convert 01.jpg -rotate 90 -resize 384 -dither FloydSteinberg -remap pattern:gray50 01F.png
 import time
+import os
 import subprocess
 import pyinotify
+from Adafruit_Thermal import *
+
 #INIT
+printer = Adafruit_Thermal("/dev/serial0", 9600, timeout=5)
 
 # The watch manager stores the watches and provides operations on watches
 wm = pyinotify.WatchManager()
@@ -11,11 +15,26 @@ mask = pyinotify.IN_CLOSE_WRITE  # watched events
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
-        print "Uploaded:", event.pathname
+        if event.pathname.endswith("_FloydSteinberg-converted.png"):
+            #print
+            print "Printing:", event.pathname
+            printer.printImage(Image.open(envent.pathname), True)
+
+        if event.pathname.endswith(".png") or event.pathname.endswith(".jpg") or event.pathname.endswith(".PNG") or event.pathname.endswith(".JPG"):
+            #convert
+            print "Uploaded:", event.pathname
+            subprocess.Popen(['convert', event.pathname, '-rotate', '90', '-resize', '384', '-dither', 'FloydSteinberg', '-remap', 'pattern:gray50', event.pathname + "_FloydSteinberg-converted.png"])
+            
+
+        if event.name == "shutdown.txt":
+            print "shutdown"
+            subprocess.call(["sudo", "shutdown", "now"])
 
 
+
+#RUN
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
 
-wdd = wm.add_watch('/home/pi/bluetoothUpload', mask, rec=True)
+wdd = wm.add_watch('/home/pi/bluetoothUpload', mask)
 notifier.loop()
