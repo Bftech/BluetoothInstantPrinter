d87663d6483f2e66a76943d453067dcd65d35cdf
Corentin Monmasson (aka Bftech) <corentin.monmasson@gmail.com>
2017-07-27 17:20:21 +0200

    u

--- BiP_imageConverter.py
+++ BiP_imageConverter.py
@@ -1,5 +1,39 @@
 # convert 01.jpg -rotate 90 -resize 384 -dither FloydSteinberg -remap pattern:gray50 01F.png
 
 import subprocess
-
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 #INIT
+
+class ConversionObserver(PatternMatchingEventHandler):
+    patterns = ["*.png", "*.jpg"]
+
+    def process(self, event):
+        """
+        event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+        # the file will be processed there
+        print event.src_path, event.event_type  # print now only for degug
+
+    def on_created(self, event):
+        self.process(event)
+
+
+#RUN
+if __name__ == '__main__':
+    convObserver = Observer()
+    convObserver.schedule(ConversionObserver(), path="/home/pi/bluetoothUpload")
+    convObserver.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        convObserver.stop()
+
+    convObserver.join()
