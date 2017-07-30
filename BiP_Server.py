1ef41595cf2b72fc5876a5e1eaa8af66008dcaba
Corentin Monmasson (aka Bftech) <corentin.monmasson@gmail.com>
2017-07-30 21:15:39 +0200

    u

--- BiP_Server.py
+++ BiP_Server.py
@@ -6,7 +6,7 @@ import pyinotify
 from PIL import Image
 from Adafruit_Thermal import *
 
-#INIT
+# INIT
 printer = Adafruit_Thermal("/dev/serial0", 9600, timeout=5)
 
 # The watch manager stores the watches and provides operations on watches
@@ -14,18 +14,20 @@ wm = pyinotify.WatchManager()
 
 mask = pyinotify.IN_CLOSE_WRITE  # watched events
 
+
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
         if event.pathname.endswith("_FloydSteinberg-converted.png"):
-            #print
+            # print
             print "Printing:", event.pathname
             printer.printImage(Image.open(event.pathname), True)
 
         if not event.pathname.endswith("_FloydSteinberg-converted.png") and event.pathname.endswith(".png") or event.pathname.endswith(".jpg") or event.pathname.endswith(".PNG") or event.pathname.endswith(".JPG"):
-            #convert
+            # convert
             print "Uploaded:", event.pathname
             print "Converting..."
-            subprocess.call(['convert', event.pathname, '-rotate', '90', '-resize', '384', '-dither', 'FloydSteinberg', '-remap', 'pattern:gray50', event.pathname + "_FloydSteinberg-converted.png"])
+            subprocess.call(['convert', event.pathname, '-rotate', '90', '-resize', '384', '-dither',
+                             'FloydSteinberg', '-remap', 'pattern:gray50', event.pathname + "_FloydSteinberg-converted.png"])
             print "Converted !"
 
         if event.name == "shutdown.txt":
@@ -34,7 +36,7 @@ class EventHandler(pyinotify.ProcessEvent):
 
 
 
-#RUN
+# RUN
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
 
